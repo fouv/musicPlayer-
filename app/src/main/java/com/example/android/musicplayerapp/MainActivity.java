503336a0b93d8d1a4173f527fe55ffa96316b264
Florence Bouvier <florencebres1@gmail.com>
2017-09-11 17:31:07 +0200

    volume SeekBar add

---
+++ app/src/main/java/com/example/android/musicplayerapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.android.musicplayerapp;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import static android.media.AudioManager.STREAM_MUSIC;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    AudioManager audioManager;
+    private Button b1, b2;
+    private MediaPlayer mediaPlayer;
+    private double startTime = 0;
+    private double finalTime = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.play);
+        b2 = (Button) findViewById(R.id.pause);
+
+        /**
+         * new mediaPlayer object
+         */
+        mediaPlayer = MediaPlayer.create(this, R.raw.coldplay);
+
+         // start mediaPlayer on click on Button Play
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                finalTime = mediaPlayer.getDuration();
+                startTime = mediaPlayer.getCurrentPosition();
+                b2.setEnabled(true);
+            }
+        });
+
+        //Pause on click second button
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+                b1.setEnabled(true);
+            }
+        });
+
+        // id seekbar + button
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        //TextView textView = (TextView) findViewById(textView1);
+
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        seekBar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+
+        //change volume setBar
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                audioManager.setStreamVolume(STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
